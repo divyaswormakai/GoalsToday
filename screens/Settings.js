dd166ba95b3d3257c08498591603a7cb35a077d6
Divyaswor Makai <makaidivya@gmail.com>
2020-05-11 19:38:40 +0545

    Before notification integration with android gradle

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,11 +1,34 @@
 import React, {useState, useEffect} from 'react';
 import {Text, View, Button} from 'react-native';
 import {globalStyles} from '../globals/globalStyles';
+import TimePicker from 'react-native-24h-timepicker';
 
 export default function Settings() {
+	const [time, setTime] = useState('');
+	let timePicker = null;
+	const onCancel = () => {
+		timePicker.close();
+	};
+
+	const onConfirm = (hour, minute) => {
+		console.log('TIMEEE');
+		console.log(typeof hour, minute);
+		setTime(hour + ':' + minute);
+		timePicker.close();
+	};
+
 	return (
 		<View style={globalStyles.container}>
 			<Text>This is settings text</Text>
+			<Button title="OpenClock" onPress={() => timePicker.open()} />
+			<Text>{time}</Text>
+			<TimePicker
+				ref={ref => {
+					timePicker = ref;
+				}}
+				onCancel={() => onCancel()}
+				onConfirm={(hour, minute) => onConfirm(hour, minute)}
+			/>
 		</View>
 	);
 }
